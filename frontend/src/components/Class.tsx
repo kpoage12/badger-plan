3cbe6451487f0046c5b214af07f5354515c30e66
Kyle Poage <kylepoage2003@gmail.com>
2025-12-23 13:17:55 -0800

    Adding modals in browse

--- frontend/src/components/Class.tsx
+++ frontend/src/components/Class.tsx
@@ -1,5 +1,7 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import { useState } from "react";
+import Modal from "react-bootstrap/esm/Modal";
 
 interface ClassProps {
   name: string;
@@ -8,6 +10,11 @@ interface ClassProps {
 }
 
 function Class({ name, subtitle, credits }: ClassProps) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <Card className="mt-2" style={{ width: "18rem" }}>
       <Card.Body>
@@ -16,9 +23,35 @@ function Class({ name, subtitle, credits }: ClassProps) {
           <Card.Subtitle className="mb-2 text-muted">{subtitle}</Card.Subtitle>
           <Card.Text className="fw-semibold">{credits} credits</Card.Text>
         </div>
-        <Button variant="outline-primary" className="mt-2 w-100">
+        <Button
+          variant="outline-primary"
+          className="mt-2 w-100"
+          onClick={handleShow}
+        >
           More Details
         </Button>
+        <div
+          className="modal show"
+          style={{ display: "block", position: "initial" }}
+        >
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>{name}</Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+              {subtitle}
+              <br />
+              {credits} credits
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button variant="primary" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
       </Card.Body>
     </Card>
   );
