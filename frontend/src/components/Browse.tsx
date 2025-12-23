d9106e0676cd93a2a9dc745b153fb38c48c8c849
Kyle Poage <kylepoage2003@gmail.com>
2025-12-23 09:49:07 -0800

    Card styling + bootstrap grid layout for classes

--- frontend/src/components/Browse.tsx
+++ frontend/src/components/Browse.tsx
@@ -1,18 +1,20 @@
 import Class from "./Class";
 import classes from "../data/data";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Browse() {
   return (
-    <div className="d-flex gap-3 overflow-auto pb-3">
-      {classes.map((c) => (
-        <Class
-          key={c.name}
-          name={c.name}
-          subtitle={c.subtitle}
-          credits={c.credits}
-        />
-      ))}
-    </div>
+    <Container>
+      <Row>
+        {classes.map((c) => (
+          <Col key={c.name} xs={12} sm={12} md={6} lg={4} xl={3}>
+            <Class name={c.name} subtitle={c.subtitle} credits={c.credits} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
--- frontend/src/data/data.tsx
+++ frontend/src/data/data.tsx
@@ -1,7 +1,27 @@
 const classes = [
   {
-    name: "CS 400",
+    name: "Comp Sci 400",
     subtitle: "Programming III",
+    credits: 3,
+  },
+  {
+    name: "Comp Sci 475",
+    subtitle: "Combinatorics",
+    credits: 3,
+  },
+  {
+    name: "Comp Sci 300",
+    subtitle: "Programming II",
+    credits: 3,
+  },
+  {
+    name: "Comp Sci 537",
+    subtitle: "Operating Systems",
+    credits: 4,
+  },
+  {
+    name: "Comp Sci 571",
+    subtitle: "Building User Interfaces",
     credits: 3,
   },
   {
